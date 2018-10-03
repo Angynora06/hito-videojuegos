38ccd096e285747c180796b2145b0a6ad93c1142
Angela <angela.novoa.ramos@alumnos.upm.es>
2018-10-03 23:25:21 +0100

    Bowsette se mueve en todas las direcciones y aparece la estrella random

---
+++ Bowsette.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bowsette : MonoBehaviour
+{
+    public float speed = 20f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movimiento horizontal
+        float hInput = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(hInput * speed * Time.deltaTime, 0, 0);
+
+        //Movimiento vertical
+        float vInput = Input.GetAxis("Vertical");
+        transform.position += new Vector3(0, vInput * speed * Time.deltaTime, 0);
+
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "estrella")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+
+}
---
+++ estrella.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class estrella : MonoBehaviour
+{
+
+
+    // Use this for initialization
+    void Start()
+    {
+        var numero = Random.Range(1, 50);
+        if (numero < 25)
+        {
+            transform.localScale = new Vector3(0f, 0f, 0f);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
